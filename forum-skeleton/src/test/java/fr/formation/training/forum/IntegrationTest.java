3186b4cfdb36101e69abb930cf255481e9a80310
Fabien Massucco <f9bm9s@gmail.com>
2020-12-17 13:25:32 +0100

    Config test environment

---
+++ forum-skeleton/src/test/java/fr/formation/training/forum/IntegrationTest.java
@@ -0,0 +1,23 @@
+package fr.formation.training.forum;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class IntegrationTest {
+
+    @Test
+    void failServer() {
+        fail();
+    }
+
+}
