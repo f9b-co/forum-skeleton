94e09291d608701fb3184b0bcc81a007822bf14b
Frank MARSHALL <frank@readresolve.io>
2021-01-12 17:31:40 +0100

    Added ObjectMapper property access levels

--- forum-skeleton/src/main/java/fr/formation/training/forum/dtos/DiscussionViewDto.java
+++ forum-skeleton/src/main/java/fr/formation/training/forum/dtos/DiscussionViewDto.java
@@ -7,18 +7,25 @@ public class DiscussionViewDto {
     private final QuestionViewDto question;
 
     private final List<AnswerViewDto> answers;
+    // private String fieldAccess = "fieldAccess";
 
     public DiscussionViewDto(QuestionViewDto question,
 	    List<AnswerViewDto> answers) {
 	this.question = question;
 	this.answers = answers;
     }
-
-    public QuestionViewDto getQuestion() {
-	return question;
-    }
-
-    public List<AnswerViewDto> getAnswers() {
-	return answers;
-    }
+    // public QuestionViewDto getQuestion() {
+    // return question;
+    // }
+    //
+    // public List<AnswerViewDto> getAnswers() {
+    // return answers;
+    // }
+    // public String getMethodAccess() {
+    // return "getMethodAccess";
+    // }
+    //
+    // private String getFieldAccess() {
+    // return "fieldAccess";
+    // }
 }
