6f99b97e35914a64323779aa634c14bd51ead7fe
Frank MARSHALL <frank@readresolve.io>
2021-01-14 12:27:25 +0100

    Added TechnologyCustomRepository

---
+++ forum-skeleton/src/main/java/fr/formation/training/forum/repositories/TechnologyCustomRepository.java
@@ -0,0 +1,34 @@
+package fr.formation.training.forum.repositories;
+
+import javax.persistence.*;
+
+import org.springframework.stereotype.Repository;
+
+import fr.formation.training.forum.dtos.TechnologyAddDto;
+
+@Repository
+public class TechnologyCustomRepository {
+
+    private final EntityManager em;
+
+    public TechnologyCustomRepository(EntityManager em) {
+	this.em = em;
+    }
+
+    public void insertConcat(TechnologyAddDto dto) {
+	String sql = "insert into technologies (technology_name, technology_rating) values (";
+	sql += "\"" + dto.getName() + "\", ";
+	sql += dto.getRating();
+	sql += ")";
+	Query query = em.createNativeQuery(sql);
+	query.executeUpdate();
+    }
+
+    public void insertParameterized(TechnologyAddDto dto) {
+	String sql = "insert into technologies (technology_name, technology_rating) values (:name, :rating)";
+	Query query = em.createNativeQuery(sql);
+	query.setParameter("name", dto.getName());
+	query.setParameter("rating", dto.getRating());
+	query.executeUpdate();
+    }
+}
