01dffaf1f927c998569a26f510cea28d9a391743
Fabien Massucco <f9bm9s@gmail.com>
2021-02-10 11:55:31 +0100

    Added RuntimeException & custom logger in CustomControllerAdvice logback-spring.xml config

--- forum-skeleton/src/main/java/fr/formation/training/forum/CustomControllerAdvice.java
+++ forum-skeleton/src/main/java/fr/formation/training/forum/CustomControllerAdvice.java
@@ -1,5 +1,7 @@
 package fr.formation.training.forum;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +13,18 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class CustomControllerAdvice extends ResponseEntityExceptionHandler {
 
+    private final static Log CUSTOMLOGGER = LogFactory.getLog(CustomControllerAdvice.class);
+
     @ExceptionHandler(RessourceNotFoundException.class)
     protected ResponseEntity<Object> handleRessourceNotFoundException (RessourceNotFoundException ex, WebRequest request) {
-        return handleExceptionInternal(ex, "CustomExceptionViaCustomControllerAdvice", null, HttpStatus.NOT_FOUND, request);
+        CUSTOMLOGGER.debug("### Custom NotFoundException Msg");
+        return handleExceptionInternal(ex, "CustomNotFoundExceptionViaCustomControllerAdvice", null, HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<Object> handleRuntimeException (RuntimeException ex, WebRequest request) {
+        CUSTOMLOGGER.error("### Custom RuntimeException Msg");
+        return handleExceptionInternal(ex, "CustomRuntimeExceptionViaCustomControllerAdvice", null, HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 
     @Override
